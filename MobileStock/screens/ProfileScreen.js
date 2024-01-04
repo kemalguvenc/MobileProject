8a6e3a44439aa7b4e1e3a7a0f0237e3136323cf3
Muhammet Kemal Güvenç <kemal.guvenc@ogr.sakarya.edu.tr>
2024-01-04 07:34:45 +0300

    Profil Yönetimi

---
+++ MobileStock/screens/ProfileScreen.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const ProfileScreen = ({ navigation }) => {
+  // Profil verileri (örnek, gerçek veritabanından alınacak)
+  const userProfile = {
+    firstName: 'Ahmet',
+    lastName: 'Çelik',
+    gender: 'Erkek',
+    birthdate: '24/05/1996',
+    email: 'ahmet.celik@gmail.com',
+  };
+
+  const handleEditProfile = () => {
+    // Profil düzenleme ekranına yönlendir
+    navigation.navigate('EditProfile');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Kullanıcı Bilgileri</Text>
+      <View style={styles.profileInfo}>
+        <Text style={styles.label}>Ad: {userProfile.firstName}</Text>
+        <Text style={styles.label}>Soyad: {userProfile.lastName}</Text>
+        <Text style={styles.label}>Cinsiyet: {userProfile.gender}</Text>
+        <Text style={styles.label}>Doğum Tarihi: {userProfile.birthdate}</Text>
+        <Text style={styles.label}>E-posta: {userProfile.email}</Text>
+      </View>
+      <TouchableOpacity style={styles.editButton} onPress={handleEditProfile}>
+        <Text style={styles.buttonText}>Profili Düzenle</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#597E52',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333',
+  },
+  profileInfo: {
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 10,
+    color: '#333',
+  },
+  editButton: {
+    backgroundColor: 'tomato',
+    padding: 15,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: '#FFFFEC',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default ProfileScreen;
